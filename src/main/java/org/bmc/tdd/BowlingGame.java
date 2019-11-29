f090c5142544d937dc8594cc75d86dad371425c2
SpokoSpartan <wojtekspoton@gmail.com>
2019-11-29 18:43:43 +0100

    Final solution

--- src/main/java/org/bmc/tdd/BowlingGame.java
+++ src/main/java/org/bmc/tdd/BowlingGame.java
@@ -1,8 +1,95 @@
 package org.bmc.tdd;
 
+import java.util.List;
+
 public class BowlingGame {
 
-    public int getScore() {
-        return -1;
-    }
+	public int getScore(List<String> rolls) {
+		int result = 0;
+		for (int i = 0; i < 10; i++) {
+			Character firstRoll = rolls.get(i).charAt(0);
+			Character secondsRoll = null;
+
+			if (i < 8){
+				if(firstRoll.equals('X')) {
+					result += 10;
+					result += getPointsFromTwoNextRolls(rolls.get(i+1), rolls.get(i+2));
+				} else {
+					secondsRoll = rolls.get(i).charAt(1);
+					int x = parseRoll(firstRoll, secondsRoll);
+					result += x;
+					result += parseRoll(secondsRoll, x);
+					if(rolls.get(i).charAt(1) == '/'){
+						result += parseRoll(rolls.get(i).charAt(0));
+					}
+				}
+			}
+			if(i == 8){
+				if(firstRoll.equals('X')) {
+					result += 10;
+					result += getPointsFromTwoNextRolls(rolls.get(i+1), rolls.get(i+1));
+				} else {
+					int x = parseRoll(rolls.get(8).charAt(0));
+					result += x;
+					result += parseRoll(rolls.get(8).charAt(1), x);
+					if(rolls.get(i).charAt(1) == '/'){
+						result += parseRoll(rolls.get(i).charAt(0));
+					}
+				}
+			} else if (i == 9){
+				if(firstRoll.equals('X')) {
+					result += 10;
+					result += parseRoll(rolls.get(9).charAt(1));
+					result += parseRoll(rolls.get(9).charAt(2), parseRoll(rolls.get(9).charAt(1)));
+				} else {
+					int x = parseRoll(rolls.get(9).charAt(0));
+					result += x;
+					result += parseRoll(rolls.get(9).charAt(1), x);
+					if (rolls.get(9).length() == 3) {
+						result += parseRoll(rolls.get(9).charAt(2));
+					}
+				}
+			}
+		}
+		return result;
+	}
+
+	private int getPointsFromTwoNextRolls(String firstFrame, String secondFrame) {
+		int sum = 0;
+		if(firstFrame.charAt(0) == 'X'){
+			sum += 10;
+			if(secondFrame.charAt(0) == 'X'){
+				sum += 10;
+			} else {
+				sum += Integer.parseInt(secondFrame.substring(0,1));
+			}
+		} else {
+			int previous =Integer.parseInt(firstFrame.substring(0,1));
+			sum += previous;
+			sum += parseRoll(firstFrame.charAt(1), previous);
+		}
+		return sum;
+
+	}
+
+	private int parseRoll(Character roll, int previousRoll) {
+		if (roll.equals('X')) {
+			return 10;
+		} else if(roll.equals('-')) {
+			return 0;
+		} else if (roll.equals('/')) {
+			return 10 - previousRoll;
+		} else {
+			return Integer.parseInt(String.valueOf(roll));
+		}
+	}
+
+	private int parseRoll(Character roll) {
+		if (roll.equals('X')) {
+			return 10;
+		} else {
+			return Integer.parseInt(String.valueOf(roll));
+		}
+	}
 }
+
--- src/test/java/org/bmc/tdd/BowlingGameTest.java
+++ src/test/java/org/bmc/tdd/BowlingGameTest.java
@@ -1,23 +1,45 @@
 package org.bmc.tdd;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 class BowlingGameTest {
 
-    BowlingGame theGame = new BowlingGame();
+	BowlingGame theGame = new BowlingGame();
 
-    @Test
-    public void shouldScoreOneWhenRolledOneWithJUnit() {
-        assertEquals(1, theGame.getScore());
-    }
+	@Test
+	public void shouldReturnMaxScoreFor12Strikes() {
+		assertEquals(300, theGame.getScore(Arrays.asList("X", "X", "X", "X", "X", "X", "X", "X", "X", "XXX")));
+	}
 
-    @Test
-    public void shouldScoreOneWhenRolledOneWithAssertJ() {
-        Assertions.assertThat(theGame.getScore()).isEqualTo(1);
-    }
+	@Test
+	public void shouldReturn90PointsForOnly9s() {
+		assertEquals(90, theGame.getScore(Arrays.asList("9-","9-","9-","9-","9-","9-","9-","9-","9-","9-")));
+	}
 
+	@Test
+	public void shouldReturn150PointsForOnly5s() {
+		assertEquals(150, theGame.getScore(Arrays.asList("5/","5/","5/","5/","5/","5/","5/","5/","5/","5/5")));
+	}
+
+	@Test
+	public void shouldReturn202() {
+		assertEquals(202, theGame.getScore(Arrays.asList("X", "8/", "9/", "8-", "X", "X", "9/", "9/", "X", "X9/")));
+	}
+
+	@Test
+	public void shouldReturn164() {
+		assertEquals(164, theGame.getScore(Arrays.asList("7/", "X", "X", "81", "9/", "81", "X", "8/", "8/", "61")));
+	}
+
+	@Test
+	public void shouldReturn276() {
+		assertEquals(276, theGame.getScore(Arrays.asList("X", "X", "X", "X", "X", "X", "X", "X", "8/", "XXX")));
+	}
 }
+
+
